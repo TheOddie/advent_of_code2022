67e6b72ccbece48e0a2f71c793db39b8b9e7124a
TheOddie <owenseary@gmail.com>
2022-12-06 05:30:51 +0000

    Day 6

---
+++ src/bin/day6-1.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = parse_input(include_str!("../input/day6.txt"));
+
+    let len_sequence = 4;
+
+    let mut index = len_sequence;
+    let mut s = take_slice(&input, index, len_sequence);
+    while !is_marker(s.as_slice()) {
+        index += 1;
+        s = take_slice(&input, index, len_sequence);
+    }
+
+    println!("{:?}", index);
+}
+
+fn parse_input(s: &str) -> Vec<char>
+{
+    return s.chars().collect::<Vec<char>>();
+}
+
+fn is_marker(s: &[char]) -> bool
+{
+    let mut result = HashSet::new();
+    for i in s {
+        result.insert(i.clone());
+    }
+    return result.len() == s.len();
+}
+
+fn take_slice(input: &Vec<char>, end: usize, length: usize) -> Vec<char>
+{
+    return input.iter()
+        .take(end)
+        .rev()
+        .take(length)
+        .rev()
+        .map(|x| x.clone())
+        .collect::<Vec<char>>();
+}
